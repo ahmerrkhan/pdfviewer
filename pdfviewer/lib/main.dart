06326968bfb1f22a6d6c4e24de41be022c17fdd5
Muhammad Ahmer Khan <mahmerk99@gmail.com>
2021-04-29 20:54:26 +0000

    pdf viewer
    simple pdf viewer application, need to be improved

---
+++ pdfviewer/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_plugin_pdf_viewer/flutter_plugin_pdf_viewer.dart';
+import 'package:pdfviewer/pdfscreen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: landingPage(),
+    );
+  }
+}
+
+class landingPage extends StatefulWidget {
+  @override
+  _landingPageState createState() => _landingPageState();
+}
+
+class _landingPageState extends State<landingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pdf Library.com'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: FlatButton(
+          child: Text('Shayad : Hazrat John Elia',style: TextStyle(color: Colors.white,fontSize: 15.0),),
+          color: Colors.grey[900],
+          onPressed: (){
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => pdfscreen()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ pdfviewer/lib/pdfscreen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_plugin_pdf_viewer/flutter_plugin_pdf_viewer.dart';
+
+class pdfscreen extends StatefulWidget {
+  @override
+  _pdfscreenState createState() => _pdfscreenState();
+}
+
+class _pdfscreenState extends State<pdfscreen> {
+  String pdfAsset = "assets/143_shayad-bookspk.pdf";
+  PDFDocument _doc;
+  bool _loading;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _initPdf();
+  }
+
+  _initPdf() async {
+    setState(() {
+      _loading = true;
+    });
+    final doc = await PDFDocument.fromAsset(pdfAsset);
+    setState(() {
+      _doc = doc;
+      _loading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shayad : Hazrat John Elia'),
+      ),
+      body: _loading
+          ? Center(
+        child: CircularProgressIndicator(),
+      )
+          : PDFViewer(document: _doc,),
+    );
+  }
+}
