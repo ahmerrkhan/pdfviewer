bbba3144c642d8edf9f4be4b3d79747c0300c5fb
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2021-05-06 09:42:41 +0000

    Update main.dart

--- pdfviewer/lib/main.dart
+++ pdfviewer/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter pdf',
+      title: 'Flutter pdf viewer ',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
