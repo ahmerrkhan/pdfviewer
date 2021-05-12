0b737e5db00b290856bf30b42a86f43d294cf006
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2021-05-12 00:04:39 +0000

    Update widget_test.dart

--- pdfviewer/test/widget_test.dart
+++ pdfviewer/test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:pdfviewer/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+    
     await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
