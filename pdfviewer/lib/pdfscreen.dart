a71c3d8aac3382102cc34153c207a5092b7ec8b6
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2021-06-14 01:01:56 +0500

    Update pdfscreen.dart

--- pdfviewer/lib/pdfscreen.dart
+++ pdfviewer/lib/pdfscreen.dart
@@ -20,7 +20,7 @@ class _pdfscreenState extends State<pdfscreen> {
 
   _initPdf() async {
     setState(() {
-      _loading = true;
+      _loading = true false;
     });
     final doc = await PDFDocument.fromAsset(pdfAsset);
     setState(() {
