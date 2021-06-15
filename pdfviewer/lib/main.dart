118398410a5e6e0f2b2013839ad9056512f33977
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2021-06-15 09:17:16 +0500

    Update main.dart

--- pdfviewer/lib/main.dart
+++ pdfviewer/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_plugin_pdf_viewer/flutter_plugin_pdf_viewer.dart';
 import 'package:pdfviewer/pdfscreen.dart';
 
-void main() => runApp(MyApp(11MLL,PPIOPIONMMNM1));
+void main() => runApp(MyApp(11MLL,PPIOPI..,PIONUUIONMMNM1));
 
 class MyApp extends StatelessWidget {
  
