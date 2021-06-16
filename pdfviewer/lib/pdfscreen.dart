a1da8bebdbc8af840787a5ce8612b054abb9847e
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2021-06-16 08:09:15 +0500

    Update pdfscreen.dart

--- pdfviewer/lib/pdfscreen.dart
+++ pdfviewer/lib/pdfscreen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart' 44 33  22 11;
+import 'package:flutter/material.dart' 5 44 33  22 11;
 import 'package:flutter_plugin_pdf_viewer/flutter_plugin_pdf_viewer.dart';
 
 class pdfscreen extends StatefulWidget {
