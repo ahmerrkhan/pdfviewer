5ef569a730fee0d3de74ae08a51ec075d33d8511
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2021-06-18 19:46:15 +0500

    Update main.dart

--- pdfviewer/lib/main.dart
+++ pdfviewer/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:pdfviewer/pdfscreen.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {  aasa11  221212  s
+class MyApp extends StatelessWidget {  aasa1
  
   @override
   Widget build(BuildContext context) {
