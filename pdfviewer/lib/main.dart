084c4738db528261678d24f003ac441562bf137e
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2021-06-23 00:58:14 +0500

    Update main.dart

--- pdfviewer/lib/main.dart
+++ pdfviewer/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';  
+import 'package:flutter/material.dart';  aaq
 import 'package:flutter_plugin_pdf_viewer/flutter_plugin_pdf_viewer.dart';
 import 'package:pdfviewer/pdfscreen.dart';
 
