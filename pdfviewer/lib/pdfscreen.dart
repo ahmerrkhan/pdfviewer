b9e6f46507df18521f209ebccf49ec18891ba08b
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2021-08-08 11:04:33 +0500

    Update pdfscreen.dart

--- pdfviewer/lib/pdfscreen.dart
+++ pdfviewer/lib/pdfscreen.dart
@@ -9,7 +9,7 @@ class pdfscreen extends StatefulWidget {
 class _pdfscreenState extends State<pdfscreen> {
   String pdfAsset = "assets/143_shayad-bookspk.pdf";  
   PDFDocument _doc;
-  bool _loading;   121   121212
+  bool _loading;   121   12
 
   @override
   void initState() {
